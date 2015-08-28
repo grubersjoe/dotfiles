4c28079badf14da592d27fcda05647b896b77e4d
Jonathan Gruber <gruberjonathan@gmail.com>
2015-08-28 09:08:31 +0200

    Restructured code for more clarity

--- i3/make-config.sh
+++ i3/make-config.sh
@@ -2,10 +2,20 @@
 
 # A simple way to add host specific extra configuration to i3 wm
 
+function insert_newline {
+    counter=${1:-1} # counter = $1 || 1
+    while [  $counter -gt 0 ]; do
+        echo >> config # (no check if a newline is already present)
+        let counter=counter-1 
+    done    
+}
+
 host_config="hosts/$(hostname).conf"
 cd ~/.i3/
 cat base.conf > config
-sed -i -e '$a\' config
+insert_newline 2
+echo "## Host specific config for $(hostname)" >> config
+insert_newline
 cat $host_config >> config
-sed -i -e '$a\' config
+insert_newline
 i3-msg restart
