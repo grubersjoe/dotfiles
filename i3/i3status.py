5fd0404501f37f1f335bd2d64dbbf2c953634651
Jonathan Gruber <gruberjonathan@gmail.com>
2015-10-22 16:08:38 +0200

    Changed format of statusbar

--- i3/i3status.py
+++ i3/i3status.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import subprocess
 import locale
 
@@ -7,34 +5,35 @@ from i3pystatus import Status
 
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
 
-status = Status(standalone=True)
+status = Status(standalone = True)
 
 status.register('clock',
-	format='  %a %-d. %B %H:%M KW %V')
+	format = '%a %-d. %B %H:%M'
+)
 
 status.register('mem',
-	format='  {used_mem} / {total_mem} GiB   ',
-	divisor=1024**3,
-	color='#ffffff',
-	round_size=2,
-	warn_percentage=60,
-	alert_percentage=80)
+	format = 'MEM {percent_used_mem}%  ',
+	divisor = 1024**3,
+	color = '#ffffff',
+	round_size = None,
+	warn_percentage = 70,
+	alert_percentage = 80
+)
 
 status.register('cpu_usage',
-	format='  {usage:02}% ')
+	format = 'CPU {usage:02}%  '
+)
 
 status.register('load',
-	format='  {avg1} {avg5} {avg15} ',
-	critical_limit=5.5)
+	format = 'Load {avg1} {avg5} {avg15}  ',
+	critical_limit = 5.5
+)
 
-status.register('spotify',
-	format='♪ {artist} - {title}   ')
+# status.register('spotify',
+# 	format = '[music] {artist} - {title}   '
+# )
 
 # status.register('pulseaudio',
-#     format='♪ {volume}')
-
-# Shows your CPU temperature, if you have a Intel CPU
-# status.register('temp',
-#     format='{temp:.0f}°C',)
+#     format = '♪ {volume}')
 
 status.run()
