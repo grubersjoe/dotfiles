87c31d2c03ce1ea8f25da7395ee4a6f51b998ac0
Jonathan Gruber <gruberjonathan@gmail.com>
2016-06-24 19:06:08 +0200

    Added icons (FontAwesome) to status bar

--- i3/status.py
+++ i3/status.py
@@ -5,56 +5,68 @@ from i3pystatus import Status
 
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
 
+fonts = {
+    'fixed': 'Meslo LG M DZ 11',
+    'icon': 'FontAwesome 12'
+}
+
+def format(contents, type = 'fixed'):
+    if type in fonts.keys():
+        return '<span font="' + fonts[type] + '">' + contents + '</span>'
+    else:
+        return contents
+
 status = Status(standalone = True)
 
 status.register('clock',
-	format = '%a %-d. %B %H:%M',
-	hints = {'markup': 'pango'},
+    format = '%a %-d. %B %H:%M',
 )
 
 status.register('xkblayout', 
-	format = '<span font="FontAwesome Normal 12">\uf11c</span> {name}  ',
-	uppercase = False,
-	layouts = ['us', 'de'],
-	hints = {'markup': 'pango'},
+    format = format('\uf11c', 'icon') + ' ' + format('{name}'),
+    uppercase = False,
+    layouts = ['us', 'de'],
+    hints = {'markup': 'pango'},
 )
 
 status.register('pulseaudio',
-	format = '<span font="FontAwesome Normal 12"></span> <span font="Meslo LG M DZ 11">{volume}</span>  ',
-	format_muted = '<span font="FontAwesome Normal 12"></span> <span font="Meslo LG M DZ 11">{volume}</span>  ',
-	color_muted = '#CC6666',
-	hints = {'markup': 'pango'},
+    format = format('\uf028', 'icon') + ' ' + format('{volume}') + '  ',
+    format_muted = format('\uf026', 'icon') + ' ' + format('{volume}') + '  ',
+    color_muted = '#CC6666',
+    hints = {'markup': 'pango'},
+    multi_click_timeout = 0
 )
 
 status.register('uptime',
-	format = 'UP <span font="Meslo LG M DZ 11">{hours}h {mins}m</span>  ',
-	hints = {'markup': 'pango'},
+    format = 'UP ' + format('{days}d {hours}h {mins}m') + '  ',
+    hints = {'markup': 'pango'},
 )
 
 status.register('load',
-	format = 'Load <span font="Meslo LG M DZ 11">{avg1} {avg5} {avg15}</span>  ',
-	critical_limit = 5.5,
-	hints = {'markup': 'pango'},
+    format = 'Load ' + format('{avg1} {avg5} {avg15}') +  '  ',
+    critical_limit = 5.5,
+    hints = {'markup': 'pango'},
 )
 
 status.register('mem',
-	format = 'MEM <span font="Meslo LG M DZ 11">{percent_used_mem}</span>%  ',
-	divisor = 1024**3,
-	color = '#ffffff',
-	round_size = None,
-	warn_percentage = 70,
-	alert_percentage = 85,
-	hints = {'markup': 'pango'},
+    format = 'MEM ' + format('{percent_used_mem}') + '%  ',
+    divisor = 1024**3,
+    color = '#ffffff',
+    round_size = None,
+    warn_percentage = 70,
+    alert_percentage = 85,
+    hints = {'markup': 'pango'},
 )
 
 status.register('cpu_usage',
-	format = 'CPU <span font="Meslo LG M DZ 11">{usage:02}</span>%  ',
-	hints = {'markup': 'pango'},
+    format = 'CPU ' + format('{usage:02}') + '%  ',
+    hints = {'markup': 'pango'},
 )
 
 status.register('spotify',
-	format = '{artist} - {title}  ',
-	format_not_running = '',
+    format = format('\uf025', 'icon') + '  {artist} - {title}  ',
+    format_not_running = '',
+    hints = {'markup': 'pango'},
 )
 
 status.run()
