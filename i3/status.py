751c5cb8af2f77b6a6cefd189600ed2f591a4626
Jonathan Gruber <gruberjonathan@gmail.com>
2016-10-24 01:08:48 +0200

    Switched to i3-gaps, various subsequent config changes

--- i3/status.py
+++ i3/status.py
@@ -6,26 +6,33 @@ from os.path import expanduser
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
 
 fonts = {
-    'sans': 'Fira Sans Regular 13',
-    'mono': 'Meslo LG M DZ 11',
-    'icon': 'Material Icons 13',
+    'sans': {
+        'typeface': 'Fira Sans Regular 13',
+        'rise': 0
+    },
+    'mono': {
+        'typeface': 'Meslo LG M DZ 11',
+        'rise': 0
+    },
+    'icon': {
+        'typeface': 'Material Icons 13',
+        'rise': -2500
+    }
 }
-icon_rise = -2500
 
 
 def spacer(width=6):
     return '<span font="Fira Sans Regular 6">' + (' ' * width) + '</span>'
 
+
 def font(contents, font='mono'):
     if font in fonts.keys():
-        if font == 'icon':
-            span = '<span font="' + fonts[font] + '" rise="' + str(icon_rise) + '">'
-        else:
-            span = '<span font="' + fonts[font] + '">'
+        span = '<span font="' + str(fonts[font]['typeface']) + '" rise="' + str(fonts[font]['rise']) + '">'
 
         return span + contents + '</span>'
     else:
         return contents
+
 
 @get_module
 def xkblayout_leftclick(self):
@@ -44,9 +51,9 @@ status.register(
 status.register(
     'xkblayout',
     format=font('\ue312', 'icon') + font(' ', 'sans') + font('{name}') + spacer(),
+    hints={'markup': 'pango'},
     uppercase=False,
     layouts=['us', 'de'],
-    hints={'markup': 'pango'},
     on_leftclick=[xkblayout_leftclick]
 )
 
@@ -54,8 +61,8 @@ status.register(
     'pulseaudio',
     format=font('\ue050', 'icon') + font(' ', 'sans') + font('{volume}') + spacer(),
     format_muted=font('\ue04f', 'icon') + font(' ', 'sans') + font('{volume}') + spacer(),
-    color_muted='#CC6666',
     hints={'markup': 'pango'},
+    color_muted='#CC6666',
     multi_click_timeout=0,
 )
 
@@ -68,19 +75,20 @@ status.register(
 status.register(
     'load',
     format=font('Load ', 'sans') + font('{avg1} {avg5} {avg15}') + spacer(),
-    critical_limit=5.5,
     hints={'markup': 'pango'},
+    critical_limit=5.5,
 )
 
 status.register(
     'mem',
     format=font('MEM ', 'sans') + font('{percent_used_mem}') + font('%', 'sans') + spacer(),
+    hints={'markup': 'pango'},
     divisor=1024 ** 3,
     color='#ffffff',
     round_size=None,
     warn_percentage=70,
-    alert_percentage=85,
-    hints={'markup': 'pango'},
+    alert_percentage=80,
+    interval=1 # does not update otherwise. bug?
 )
 
 status.register(
@@ -92,12 +100,12 @@ status.register(
 status.register(
     'now_playing',
     format=font('{status} {artist} - {title}', 'sans') + spacer(),
+    hints={'markup': 'pango'},
     status={
         'stop': font('\ue037', 'icon'),
         'pause': font('\ue037', 'icon'),
         'play': font('\ue034', 'icon')
     },
-    hints={'markup': 'pango'},
     on_leftclick=None,
     on_doubleleftclick='playpause',
 )
