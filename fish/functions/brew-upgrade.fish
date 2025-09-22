208fcfdc6d7f5aef7a955b8c6511dd2fcb0c38f6
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2025-09-22 10:33:13 +0200

    Add brew-upgrade function

---
+++ fish/functions/brew-upgrade.fish
@@ -0,0 +1,10 @@
+function brew-upgrade
+    # Workaround for mac OS behavior to auto-remove apps from the dock.
+    # See https://github.com/Homebrew/homebrew-cask/issues/102721
+    defaults export com.apple.dock /tmp/dock.defaults
+
+    brew upgrade --greedy-auto-updates
+
+    defaults import com.apple.dock /tmp/dock.defaults
+    killall Dock
+end
