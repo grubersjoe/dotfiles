4c67e61a86900528a65f81a855c19d6ee439e0ae
Jonathan Gruber <jonathan.gruber@gridfuse.com>
2026-01-08 13:48:26 +0100

    Add Fish migration files

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,57 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 7596E4
+set --global fish_color_cancel --reverse
+set --global fish_color_command 164CC9
+set --global fish_color_comment 007B7B
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end 02BDBD
+set --global fish_color_error 9177E5
+set --global fish_color_escape 00a6b2
+set --global fish_color_hg_added green
+set --global fish_color_hg_clean green
+set --global fish_color_hg_copied magenta
+set --global fish_color_hg_deleted red
+set --global fish_color_hg_dirty red
+set --global fish_color_hg_modified yellow
+set --global fish_color_hg_renamed magenta
+set --global fish_color_hg_unmerged red
+set --global fish_color_hg_untracked yellow
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword 164CC9
+set --global fish_color_match --background=brblue
+set --global fish_color_normal normal
+set --global fish_color_operator 00a6b2
+set --global fish_color_option 4319CC
+set --global fish_color_param 4319CC
+set --global fish_color_quote 4C3499
+set --global fish_color_redirection 248E8E
+set --global fish_color_search_match bryellow --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
